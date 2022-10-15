1e334360b5424c2d7f2b7ae19fcfeb24d6f42fc4
Nguyễn Văn Thuận <nguyenthuankma@gmail.com>
2022-10-15 18:58:36 +0700

    update: facke-api + product slider

--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -1,10 +1,43 @@
-import React from 'react'
-import './product.css'
+import React, { Component } from "react";
+import Slider from "react-slick";
+import "../../../node_modules/slick-carousel/slick/slick.css"
+import "../../../node_modules/slick-carousel/slick/slick-theme.css"
+// import "../slick-carousel/slick/slick.css"; 
+// import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 const Product = () => {
-  return (
-    <h1>index</h1>
-  )
-}
+  // Setting Slider
+  const settings = {
+    dots: true,
+  };
 
-export default Product
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12">
+          <div className="page-content">
+            <Slider {...settings}>
+              <div>
+                <img src="http://placekitten.com/g/400/200" />
+              </div>
+              <div>
+                <img src="http://placekitten.com/g/400/200" />
+              </div>
+              <div>
+                <img src="http://placekitten.com/g/400/200" />
+              </div>
+              <div>
+                <img src="http://placekitten.com/g/400/200" />
+              </div>
+            </Slider>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
